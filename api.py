d4738014182d8cc3fb294c661ae5fd0586bbe82c
Drupad Khublani <khublani@gmail.com>
2018-09-02 19:21:19 -0500

    Update api.py

--- api.py
+++ api.py
@@ -35,58 +35,6 @@ graph = tf.get_default_graph()
 app = Flask(__name__)
 
 
-def extract_car(img, obj, bound=0.15):
-    # TODO add that to an ImageAPIpy
-    '''Extract the car from an image. With bound parameter, image can be larger then
-    what the bbox indicate.'''
-    x = obj['bottomright']['x'] - obj['topleft']['x']
-    y = obj['bottomright']['y'] - obj['topleft']['y']
-    topleft_y = math.floor(obj['topleft']['y'] - (y * bound))
-    topleft_x = math.floor(obj['topleft']['x'] - (x * bound))
-    botright_y = math.floor(obj['bottomright']['y'] + (y * bound))
-    botright_x = math.floor(obj['bottomright']['x'] + (x * bound))
-
-    if topleft_y < 0:
-        topleft_y = 0
-    if topleft_x < 0:
-        topleft_x = 0
-    if botright_x > img.shape[1]:
-        botright_x = img.shape[1]
-    if botright_y > img.shape[0]:
-        botright_y = img.shape[0]
-
-    topleft_x = int(topleft_x)
-    topleft_y = int(topleft_y)
-    botright_x = int(botright_x)
-    botright_y = int(botright_y)
-
-    cropped = img[topleft_y:botright_y, topleft_x:botright_x]
-    return cropped
-# TODO prediction endpoint
-# TODO Info endpoint
-#
-
-
-@app.route('/car', methods=["POST"])
-def car():
-    '''Overall prediction'''
-    global graph
-    with graph.as_default():
-        data = request.get_json()
-        b64img = Image.open(BytesIO(base64.b64decode(data['b64_image'])))
-        img = pil_to_ndarray(b64img)
-
-        img = resize(img, (299, 299))
-
-        pred = auto_model.predict(img[None, :, :, :])
-
-        classe = np.argmax(pred[0])
-        resp = jsonify({"Make": car_dict[str(classe)]['Make'],
-                        "Model": car_dict[str(classe)]['Model'],
-                        "Prob": float(np.max(pred[0]))})
-
-    return resp
-
 @app.route('/find_objects', methods=["POST"])
 def find_object():
     global graph
